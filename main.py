fc8ae586a4fea9f0fd9e98d4c23c12da46d811f0
delphox60 <delphox60@gmail.com>
2023-03-21 18:27:56 +0900

    feat: parse date and event

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 from bs4 import BeautifulSoup
 
 academic_calendar_soup = BeautifulSoup(open("UNIST-academic-calendar-2023.html", "r").read(), "html.parser")
@@ -5,3 +6,25 @@ academic_calendar_soup = BeautifulSoup(open("UNIST-academic-calendar-2023.html",
 semestor_tag = academic_calendar_soup.find("div", attrs = {"class":"semester_cal_ro year2023"})
 
 monthly_tags = semestor_tag.find_all("tbody")
+
+# print(monthly_tags[-2])
+
+year = 2023
+
+for tag in monthly_tags:
+    month = tag.find("th", attrs = {"class":"tac"}).text
+    
+    if "년" in month:
+        split_text = month.split()
+        year = split_text[0][:-1]
+        month = split_text[1]
+        
+    events = tag.find_all("tr")
+    
+    for event in events:
+        td_tags = event.find_all("td")
+        if len(td_tags) == 0:
+            continue
+        
+        date = td_tags[0].text
+        event_name = td_tags[1].text
