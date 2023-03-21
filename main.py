ac873b4da226952859fbccd696a7dbc0721f4e5f
delphox60 <delphox60@gmail.com>
2023-03-21 18:11:15 +0900

    feat: parse tags monthly

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from bs4 import BeautifulSoup
 
 academic_calendar_soup = BeautifulSoup(open("UNIST-academic-calendar-2023.html", "r").read(), "html.parser")
+
+semestor_tag = academic_calendar_soup.find("div", attrs = {"class":"semester_cal_ro year2023"})
+
+monthly_tags = semestor_tag.find_all("tbody")
