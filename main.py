fa30ec10853224a21994c411b886f5483713df91
delphox60 <delphox60@gmail.com>
2023-03-22 14:08:54 +0900

    style: code formatting

--- main.py
+++ main.py
@@ -4,54 +4,84 @@ import pytz
 from bs4 import BeautifulSoup
 from icalendar import Calendar, Event, vCalAddress, vText
 
+
 def parse_date(year, month, date):
     ymd_match = re.compile("(20[0-9]+)년[ ]*([0-9]+)월[ ]*([0-9]+)일.*").match(date)
     if ymd_match:
-        return datetime(int(ymd_match.group(1)), int(ymd_match.group(2)), int(ymd_match.group(3)), 0, 0, 0, tzinfo=pytz.timezone('Asia/Seoul'))
+        return datetime(
+            int(ymd_match.group(1)),
+            int(ymd_match.group(2)),
+            int(ymd_match.group(3)),
+            0,
+            0,
+            0,
+            tzinfo=pytz.timezone("Asia/Seoul"),
+        )
 
     md_match = re.compile("([0-9]+)월[ ]*([0-9]+)일.*").match(date)
     if md_match:
-        return datetime(int(year), int(md_match.group(1)), int(md_match.group(2)), 0, 0, 0, tzinfo=pytz.timezone('Asia/Seoul'))
-       
+        return datetime(
+            int(year),
+            int(md_match.group(1)),
+            int(md_match.group(2)),
+            0,
+            0,
+            0,
+            tzinfo=pytz.timezone("Asia/Seoul"),
+        )
+
     d_match = re.compile("([0-9]+)일.*").match(date)
     if d_match:
-        return datetime(int(year), int(month), int(d_match.group(1)), 0, 0, 0, tzinfo=pytz.timezone('Asia/Seoul'))
-        
+        return datetime(
+            int(year),
+            int(month),
+            int(d_match.group(1)),
+            0,
+            0,
+            0,
+            tzinfo=pytz.timezone("Asia/Seoul"),
+        )
+
+
 calendar = Calendar()
 
-academic_calendar_soup = BeautifulSoup(open("UNIST-academic-calendar-2023.html", "r").read(), "html.parser")
+academic_calendar_soup = BeautifulSoup(
+    open("UNIST-academic-calendar-2023.html", "r").read(), "html.parser"
+)
 
-semestor_tag = academic_calendar_soup.find("div", attrs = {"class":"semester_cal_ro year2023"})
+semestor_tag = academic_calendar_soup.find(
+    "div", attrs={"class": "semester_cal_ro year2023"}
+)
 
 monthly_tags = semestor_tag.find_all("tbody")
 
 year = 2023
 
 for tag in monthly_tags:
-    month = tag.find("th", attrs = {"class":"tac"}).text
-    
+    month = tag.find("th", attrs={"class": "tac"}).text
+
     if "년" in month:
         split_text = month.split()
         year = split_text[0][:-1]
         month = split_text[1]
-        
+
     events = tag.find_all("tr")
-    
+
     for event in events:
         td_tags = event.find_all("td")
         if len(td_tags) == 0:
             continue
-        
+
         date = td_tags[0].text
-        events = list(map(lambda e : e.strip(), td_tags[1].text.strip().split('\n')))
-        
+        events = list(map(lambda e: e.strip(), td_tags[1].text.strip().split("\n")))
+
         # if re.compile("([0-9]+)월.*([0-9]+)일.*\~([0-9]+)월.*([0-9]+)일.*").match(date):
         period_match = re.compile("(.*)\~(.*)").match(date)
-        
+
         if period_match:
             start_date = parse_date(year, month, period_match.group(1).strip())
             end_date = parse_date(year, month, period_match.group(2).strip())
-        else:       
+        else:
             start_date = parse_date(year, month, date)
             end_date = None
 
@@ -60,15 +90,12 @@ for tag in monthly_tags:
             "start_date": start_date,
             "end_date": end_date,
         }
-        
+
         ic_event = Event()
         for event_name in event_info["events"]:
             ic_event.add("name", event)
             ic_event.add("dtstart", event_info["start_date"])
             if event_info["end_date"]:
                 ic_event.add("dtend", event_info["end_date"])
-            
-            calendar.add_component(event)
 
-            
-        
+            calendar.add_component(event)
